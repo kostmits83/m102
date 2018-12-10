0fab0413f8db4f82b685b816ed7f122cb491f045
Charalabos Vairlis <h.vairlis@gmail.com>
2018-12-10 19:51:35 +0200

    Create contact page html skeleton

--- frontend/views/site/contact.php
+++ frontend/views/site/contact.php
@@ -2,6 +2,8 @@
 
 use yii\helpers\Html;
 use yii\widgets\ActiveForm;
+use yii\widgets\Breadcrumbs;
+use yii\helpers\Url;
 
 /* @var $this yii\web\View */
 /* @var $model common\models\ContactMessage */
@@ -12,21 +14,41 @@ $this->params['breadcrumbs'][] = ['label' => Yii::t('app', 'Contact Messages'), 
 $this->params['breadcrumbs'][] = $this->title;
 ?>
 <div class="contact-message-form">
-
-    <h1><?= Html::encode($this->title) ?></h1>
-
-    <?php $form = ActiveForm::begin(); ?>
-
-    <?= $form->field($model, 'name')->textInput(['maxlength' => true]) ?>
-
-    <?= $form->field($model, 'email')->textInput(['maxlength' => true]) ?>
-
-    <?= $form->field($model, 'message')->textarea(['rows' => 6]) ?>
-
-    <div class="form-group">
-        <?= Html::submitButton(Yii::t('app', 'Save'), ['class' => 'btn btn-success']) ?>
+	<div class="banner banner--light">
+    	<p class="banner__header">CONTACT US</p>
+		<p class="banner__info">Don't be shy, just send us a message!</p>
     </div>
-
-    <?php ActiveForm::end(); ?>
+	<?= Breadcrumbs::widget([
+        'links' => isset($this->params['breadcrumbs']) ? $this->params['breadcrumbs'] : [],
+    ]) ?>
+	
+	<div class="container">
+		<div class="row">
+			<div class="col-sm-12 col-md-7">
+				<p>If you really liked this project then don't hesitate to send us a message. We would be glad to hear from you!</p>
+			</div>
+			<div class="col-sm-12 col-md-5">
+				<?php $form = ActiveForm::begin(); ?>
+			    <?= $form->field($model, 'name')->textInput(['maxlength' => true]) ?>
+			    <?= $form->field($model, 'email')->textInput(['maxlength' => true]) ?>
+			    <?= $form->field($model, 'message')->textarea(['rows' => 6]) ?>
+			    <div class="form-group">
+			        <?= Html::submitButton(Yii::t('app', 'Save'), ['class' => 'btn btn-success']) ?>
+			    </div>
+			    <?php ActiveForm::end(); ?>
+			</div>
+		</div>
+	</div>
+	
+	<div>
+		<div class="container">
+			<ul>
+                <li><a href="https://www.linkedin.com/in/konstantinos-mitsarakis-a9768350/">Konstantinos Mitsarakis</a></li>
+                <li><a href="https://www.linkedin.com/in/charalabos-vairlis/">Charalampos Vairlis</a></li>
+                <li><a href="https://www.linkedin.com/">Dan Šilhavý</a></li>
+                <li><a href="http://msc.it.teithe.gr/">MSc in Web Intelligence</a></li>
+            </ul>
+		</div>
+	</div>
 
 </div>
