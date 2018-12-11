68eed56f068421a1bfe68b3b845901a2c001d21f
Kostas Mitsarakis <kostas.mitsarakis@gmail.com>
2018-12-11 18:14:22 +0200

    Create scroll to top link

---
+++ common/web/js/main.js
@@ -0,0 +1,24 @@
+﻿
+// Show or hide the scroll link
+$(window).scroll(function () {
+	if ($(this).scrollTop() > 100) {
+		$(".scroll-to-top").fadeIn();
+	} else {
+		$(".scroll-to-top").fadeOut();
+	}
+});
+
+// Animate the scroll duration
+$(".scroll-to-top").click(function () {
+	$("body, html").animate({
+		scrollTop: 0
+	}, 800);
+	return false;
+});
+
+// Open links in new tab
+$(".js-external").click(function() {
+	var target = $(this).attr("href");
+	window.open(target);
+	return false;
+});
