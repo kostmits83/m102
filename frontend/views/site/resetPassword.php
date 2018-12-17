5238da999bf1739f455c04f2e61f445ca88fe52a
Kostas Mitsarakis <kostas.mitsarakis@gmail.com>
2018-12-17 23:43:22 +0200

    Add reset password css

--- frontend/views/site/resetPassword.php
+++ frontend/views/site/resetPassword.php
@@ -6,26 +6,34 @@
 
 use yii\helpers\Html;
 use yii\bootstrap\ActiveForm;
+use yii\widgets\Breadcrumbs;
 
 $this->title = 'Reset password';
 $this->params['breadcrumbs'][] = $this->title;
 ?>
 <div class="site-reset-password">
-    <h1><?= Html::encode($this->title) ?></h1>
-
-    <p>Please choose your new password:</p>
-
-    <div class="row">
-        <div class="col-lg-5">
-            <?php $form = ActiveForm::begin(['id' => 'reset-password-form']); ?>
-
-                <?= $form->field($model, 'password')->passwordInput(['autofocus' => true]) ?>
-
-                <div class="form-group">
-                    <?= Html::submitButton('Save', ['class' => 'btn btn-primary']) ?>
-                </div>
-
-            <?php ActiveForm::end(); ?>
+    <div class="banner banner--light banner--main">
+        <p class="banner__header">RESET PASSWORD</p>
+        <p class="banner__info">Reset your password.</p>
+    </div>
+    <?= Breadcrumbs::widget([
+        'links' => isset($this->params['breadcrumbs']) ? $this->params['breadcrumbs'] : [],
+    ]) ?>
+    
+    <div class="container">
+        <div class="row">
+            <div class="col-sm-12 col-md-5 reset-password-message">
+                <p class="reset-password-message__text">Please choose your new password to change it.</p>
+            </div>
+            <div class="col-sm-12 col-md-5 col-md-offset-2 reset-password-form">
+                <h1 class="reset-password__header header-2">Reset Password Form</h1>
+                <?php $form = ActiveForm::begin(['id' => 'form-reset-password']); ?>
+                    <?= $form->field($model, 'password')->passwordInput(['placeholder' => 'Your Password']) ?>
+                    <div class="reset-password-form__button">
+                        <?= Html::submitButton(Yii::t('app', 'Save'), ['class' => 'btn button--attention button buttons-row__button']) ?>
+                    </div>
+                <?php ActiveForm::end(); ?>
+            </div>
         </div>
     </div>
 </div>
