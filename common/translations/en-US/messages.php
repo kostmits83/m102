f469138643d51b962ebde01b9c378ac26d14dea0
Kostas Mitsarakis <kostas.mitsarakis@gmail.com>
2019-01-13 20:57:38 +0200

    Minor fixes 2

--- common/translations/en-US/messages.php
+++ common/translations/en-US/messages.php
@@ -7,5 +7,6 @@ return [
     'data_already_saved' => 'You have already saved this kind of data!',
     'data_not_saved_check_values' => 'Data could not been saved. Please check your input values.',
     'data_deleted' => 'Data have been deleted successfully!',
+    'you_need_to_login_to_perform_this_action' => 'You need to login to perform this action.',
     'page_404' => 'The requested page does not exist.',
 ];
